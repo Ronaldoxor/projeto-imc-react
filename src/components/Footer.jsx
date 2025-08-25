89713de540f5c90bedae8962bfdd373e56c1d006
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-25 09:27:52 -0300

    Alterando o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div className="footer">
-            <h5>Dsenvolvido por: Ronaldo Aparecido Monteiro Almeida v.1.0</h5>
+            <h5>Desenvolvido por: Ronaldo Aparecido Monteiro Almeida v.1.0</h5>
             <p>&copy; 2025 -todos os direitos reservados </p>
         </div>
     )
